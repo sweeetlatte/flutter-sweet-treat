5b1ab8f6957b39d2186df373aba14bf066ff6331
sweeetlatte <thuylinh271055@gmail.com>
2022-11-28 16:47:53 +0700

    feat: UI favorites + seen

--- lib/pages/seen/seen_body.dart
+++ lib/pages/seen/seen_body.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:sweettreat/config/const.dart';
 
 class SeenBody extends StatelessWidget {
   const SeenBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Seen body'),
+    return ListView.builder(
+      itemCount: 10,
+      padding: const EdgeInsets.fromLTRB(18, 10, 18, 18),
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          key: ValueKey<int>(index),
+          child: Container(
+            width: double.infinity,
+            height: 180,
+            padding: const EdgeInsets.only(bottom: 18),
+            child: GridTile(
+              footer: ClipRRect(
+                borderRadius:
+                    const BorderRadius.vertical(bottom: Radius.circular(9)),
+                child: Container(
+                  color: stPrimaryVariant,
+                  child: const GridTileBar(
+                    title: Text(
+                      'data',
+                      style: styleTitleBlack,
+                    ),
+                    trailing: Icon(
+                      Icons.swipe,
+                      size: sizeIconButton,
+                    ),
+                  ),
+                ),
+              ),
+              child: Container(
+                  decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(9),
+                image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage(
+                      'assets/images/chocolate-strawberry-shortcake.webp'),
+                ),
+              )),
+            ),
+          ),
+        );
+      },
     );
   }
 }
